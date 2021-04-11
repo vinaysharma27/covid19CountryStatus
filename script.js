9911f2309f50e0195f7953da526b355ec4e8880b
vinaysharma27 <vinaysharmapuch@gmail.com>
2021-04-11 16:55:27 +0530

    Add the functionality for showing data

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ function validateForm() {
       return false;
     }
 }
+
+function showData()
+{
+    event.preventDefault();
+    var country=document.getElementById("country").value;
+    var sDate=document.getElementById("start").value;
+    var eDate=document.getElementById("end").value;
+
+    var confirmed=document.getElementById("confirmed");
+    var active=document.getElementById("active");
+    var deaths=document.getElementById("deaths");
+
+    console.log(sDate);
+
+    if(country=='' || sDate=='' || eDate=='')
+    alert("enter the required field");
+
+    else
+    {
+        var url="https://api.covid19api.com/country/"+country+"?from="+sDate+"T00:00:00Z&to="+eDate+"T00:00:00Z";
+
+        fetch(url)
+        .then((wrapper) => wrapper.json())
+        .then((wrapper) => {
+            console.log(wrapper);
+            var length=wrapper.length;
+            var index=length-1;
+
+            var c=wrapper[index].Confirmed;
+            var a=wrapper[index].Active;
+            var d=wrapper[index].Deaths;
+
+            confirmed.innerHTML=c;
+            active.innerHTML=a;
+            deaths.innerHTML=d;
+
+            document.getElementById("wrapper").style.display="block";
+            
+        })
+    }
+}
